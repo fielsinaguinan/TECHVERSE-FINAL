a7df31d441b48810feaf9916c6e752e4e2983ee5
fielsinaguinan <sinaguinanfiel@gmail.com>
2025-02-12 01:16:05 +0800

    Revised Design and Implemented Back To Top Feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+// Get the button element
+const backToTopButton = document.getElementById("backToTopBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+}
+
+// Smooth scroll to top
+backToTopButton.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' 
+  });
+});
